2fba9187c91e2f7d1cb04e8452f2dc7f8317c111
daiwl <danil.ivanov.v@gmail.com>
2018-04-21 13:00:29 +0400

    tom service

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `guest` (
+CREATE TABLE if not exists `guest` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
   `fio` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
